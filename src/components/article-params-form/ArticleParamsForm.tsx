3f5479e8d8af954e52b2c80d3c8927ccfbbe551d
Andrey Ignashov <maylo7771@yandex.ru>
2024-11-20 20:38:50 +0300

    Реализована механика открытия/закрытия панели с формой

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,30 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 export const ArticleParamsForm = () => {
+	const asideRef = useRef(null);
+	const [isFormOpen, setIsFormOpen] = useState(false);
+
+	const toggleFormOpen = () => {
+		setIsFormOpen(!isFormOpen);
+	};
+
+	useOutsideClickClose({
+		rootRef: asideRef,
+		isOpen: isFormOpen,
+		onChange: setIsFormOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isFormOpen} onClick={toggleFormOpen} />
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, isFormOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
